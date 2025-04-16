85cf28cdf30df021449337c6faa943184f9c9923
umwwwelt <yann@velte.studio>
2025-04-16 17:26:57 +0200

    feat: test yellow

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -41,7 +41,7 @@ return {
 					---@diagnostic disable: undefined-field
 					return {
 						-- NVIM
-						TelescopeBorder = { fg = colors.red },
+						TelescopeBorder = { fg = colors.yellow },
 						CursorLineNr = { fg = colors.green, bold = true },
 						CursorLine = { bg = colors.trans },
 						AvanteToBeDelete = { bg = colors.red, fg = colors.cyan },
