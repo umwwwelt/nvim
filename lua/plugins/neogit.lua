f9bada20cc56e76bbd02a447bb40591115d5593e
umwwwelt <yann@velte.studio>
2025-05-19 11:38:27 +0200

    feat(neogit): add keymap to open Neogit in floating window with `<leader>gs`

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -7,4 +7,8 @@ return {
 		-- Only one of these is needed.
 		"nvim-telescope/telescope.nvim", -- optional
 	},
+
+	config = function(_)
+		vim.keymap.set("n", "<leader>gs", "<cmd>Neogit kind=floating<CR>", { desc = "Open [G]it" })
+	end,
 }
